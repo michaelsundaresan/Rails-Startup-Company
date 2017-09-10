1fc5546a2c646875e4fb5e253964455172f44d92
Michael Sundaresan <sundaresan108@gmail.com>
2017-09-10 06:02:14 +0000

    Added association for plans and users

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,5 +3,5 @@ class Contact < ActiveRecord::Base
   #Contact form validations
     validates :name, presence: true
     validates :email, presence: true
-     validates :comments, presence: true
+    validates :comments, presence: true
 end
